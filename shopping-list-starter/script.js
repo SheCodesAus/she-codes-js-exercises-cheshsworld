9265d56875562d550f41c30a972791818c9fad11
cheshsworld <94447348+cheshsworld@users.noreply.github.com>
2022-04-02 13:00:02 +0800

    continuation of intro to js

--- shopping-list-starter/script.js
+++ shopping-list-starter/script.js
@@ -1,3 +1,53 @@
-function addItem() {}
+let shoppingListItems = ["milk", "eggs", "bread"]
+let listElement = document.getElementById("shopping-list-items");
 
-function updateItems() {}
+shoppingListItems.forEach((item) => {
+    console.log(item)
+    let itemElement = document.createElement("li");
+    itemElement.innerText = item;
+    listElement.appendChild(itemElement);
+})
+
+
+function addItem() {
+    let item = document.getElementById("new-item-text");
+    shoppingListItems.push(item.value);
+    console.log(shoppingListItems);
+    updateItems()
+    item.value = "";
+}
+
+function updateItems() {
+    let listElement = document.getElementById("shopping-list-items");
+    listElement.innerHTML = "";
+    shoppingListItems.forEach((item) => {
+        let itemElement = document.createElement("li");
+        itemElement.innerText = item;
+        listElement.appendChild(itemElement);
+    });
+}
+
+updateItems();
+
+
+function clearList() {
+    shoppingListItems = [];
+    updateItems();
+}
+
+
+// function removeItem() {
+//     let listElement = document.getElementById("shopping-list-items");
+//     listElement.innerHTML = "";
+//     shoppingListItems.forEach((item) => {
+//         let itemElement = document.createElement("li");
+//         itemElement.innerText = item;
+//         listElement.appendChild(itemElement);
+// });
+// }
+
+// function clearList() {
+//     document.getElementById("shopping-list-items").innerHTML = "";
+//     shoppingListItems = [];
+// }
+
